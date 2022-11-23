8962c104e9db84216812bd6bda1d5829456c4757
john <johnny.lioup@gmail.com>
2022-11-23 17:06:33 +0000

    Added books extensions tests and updated the openapi doc to have a 400 for the put

--- src/routers/books.js
+++ src/routers/books.js
@@ -11,10 +11,27 @@ router.get("/:id", (req, res) => {
   const id = Number(req.params.id)
   const book = books.find((book) => book.id === id)
 
+  if(!book) {
+    return res.status(404).json({error: 'error'})
+  }
+
   res.json({ book })
 })
 
 router.post("/", (req, res) => {
+  
+
+  if(!req.body.title || !req.body.type || !req.body.author
+    || !req.body.topic || !req.body.publicationDate || !req.body.pages) {
+      return res.status(400).json({error: 'error'})
+  }
+
+  const bookWithTitleExists = books.find((book) => book.title === req.body.title)
+
+  if(bookWithTitleExists) {
+    return res.status(409).json({error: 'error'})
+  }
+
   bookId++
 
   const book = {
@@ -28,9 +45,42 @@ router.post("/", (req, res) => {
 })
 
 router.put("/:id", (req, res) => {
+  if(!req.body.title || !req.body.type || !req.body.author
+    || !req.body.topic || !req.body.publicationDate || !req.body.pages) {
+      return res.status(400).json({error: 'error'})
+  }
   const id = Number(req.params.id)
   const updatedBook = req.body
+
+  const bookWithTitleExists = books.find((book) => book.title === updatedBook.title)
+
+  if(bookWithTitleExists) {
+    return res.status(409).json({error: 'error'})
+  }
+
   const bookId = books.findIndex((book) => book.id === id)
+  if(bookId < 0) {
+    return res.status(404).json({error: 'error'})
+  }
+  books[bookId] = { ...books[bookId], ...updatedBook }
+
+  res.json({ book: books[bookId] })
+})
+
+router.patch("/:id", (req, res) => { 
+  const id = Number(req.params.id)
+  const updatedBook = req.body
+
+  const bookWithTitleExists = books.find((book) => book.title === updatedBook.title)
+
+  if(bookWithTitleExists) {
+    return res.status(409).json({error: 'error'})
+  }
+
+  const bookId = books.findIndex((book) => book.id === id)
+  if(bookId < 0) {
+    return res.status(404).json({error: 'error'})
+  }
   books[bookId] = { ...books[bookId], ...updatedBook }
 
   res.json({ book: books[bookId] })
@@ -40,6 +90,9 @@ router.delete("/:id", (req, res) => {
   const id = Number(req.params.id)
   const bookId = books.findIndex((book) => book.id === id)
   const book = books[bookId]
+  if(!book) {
+    return res.status(404).json({error: 'error'})
+  }
   books.splice(bookId, 1)
 
   res.json({ book })
--- test/api/extensions/books.spec.js
+++ test/api/extensions/books.spec.js
@@ -1,1 +1,105 @@
-console.log('hello')
+const supertest = require("supertest")
+const app = require("../../../src/server.js")
+const { book1, book2, book3 } = require("../../fixtures/bookData.js")
+
+describe("Books Extension Endpoint", () => {
+  describe("GET /books", () => {
+    it("will return 404 when getting a book that does not exist", async () => {
+        const response = await supertest(app).get("/books/999")
+
+        expect(response.status).toEqual(404)
+    })
+  })
+
+  describe("POST /books", () => {
+    it("will return 400 when creating a book with missing body fields", async () => {
+        const response = await supertest(app).post("/books").send({
+            title: "test"
+          })
+
+        expect(response.status).toEqual(400)
+    })
+
+    it("will return 409 when creating a book with an already existing title", async () => {
+        const response = await supertest(app).post("/books").send({
+            title: "1984",
+            type: "test1",
+            author: "test1",
+            topic: "test1",
+            publicationDate: "2020-11-16T00:00:00.000Z",
+            pages: 1
+          })
+
+        expect(response.status).toEqual(409)
+    })
+  })
+
+  describe("PUT /books", () => {
+    it("will return 400 when updating a book with missing body fields", async () => {
+        const response = await supertest(app).put("/books/999").send({
+            title: "test1",
+          })
+
+        expect(response.status).toEqual(400)
+    })
+    it("will return 404 when updating a book that does not exist", async () => {
+        const response = await supertest(app).put("/books/999").send({
+            title: "test1",
+            type: "test1",
+            author: "test1",
+            topic: "test1",
+            publicationDate: "2020-11-16T00:00:00.000Z",
+            pages: 1
+          })
+
+        expect(response.status).toEqual(404)
+    })
+    it("will return 409 when updating a book with title that already exists", async () => {
+        const response = await supertest(app).put("/books/1").send({
+            title: "1984",
+            type: "test1",
+            author: "test1",
+            topic: "test1",
+            publicationDate: "2020-11-16T00:00:00.000Z",
+            pages: 1
+          })
+
+        expect(response.status).toEqual(409)
+    })
+  })
+
+  describe("PATCH /books", () => {
+    it("will return 404 when updating a book that does not exist", async () => {
+        const response = await supertest(app).put("/books/999").send({
+            title: "test1",
+            type: "test1",
+            author: "test1",
+            topic: "test1",
+            publicationDate: "2020-11-16T00:00:00.000Z",
+            pages: 1
+          })
+
+        expect(response.status).toEqual(404)
+    })
+    it("will return 404 when updating a book with title that already exists", async () => {
+        const response = await supertest(app).put("/books/1").send({
+            title: "1984",
+            type: "test1",
+            author: "test1",
+            topic: "test1",
+            publicationDate: "2020-11-16T00:00:00.000Z",
+            pages: 1
+          })
+
+        expect(response.status).toEqual(409)
+    })
+  })
+
+  describe("DELETE /books", () => {
+    it("will return 404 when updating a book that does not exist", async () => {
+        const response = await supertest(app).delete("/books/999")
+
+        expect(response.status).toEqual(404)
+    })
+  })
+})
