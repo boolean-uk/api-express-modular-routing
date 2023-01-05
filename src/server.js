c006da667ec916e67e78c533a8aa1c44346ab765
Fatima <fxtimaahmed14@gmail.com>
2023-01-05 16:05:01 +0000

    CORE DONE

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const cors = require('cors')
-const morgan = require('morgan')
+const cors = require("cors");
+const morgan = require("morgan");
 
 // SETUP MIDDLEWARE
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 // REQUIRE ROUTERS
-const usersRouter = require('./routers/users')
-const filmsRouter = require('./routers/films')
+const usersRouter = require("./routers/users");
+const filmsRouter = require("./routers/films");
+const booksRouter = require("./routers/books");
 
 // ADD ROUTERS TO APP
-app.use('/users', usersRouter)
-app.use('/films', filmsRouter)
+app.use("/users", usersRouter);
+app.use("/films", filmsRouter);
+app.use("/books", booksRouter);
 
-module.exports = app
+module.exports = app;
