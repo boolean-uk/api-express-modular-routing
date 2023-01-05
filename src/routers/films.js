d21c72dfb39805f311ee3edf98987bf8a9216b60
akibria12 <ahkibria@hotmail.co.uk>
2023-01-05 18:04:16 +0000

    films Extension done

--- src/routers/films.js
+++ src/routers/films.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { films } = require("../../data/index.js");
 
-router.get("/", (req, res) => {
-  res.json({ films });
-});
-
 router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
   const film = films.find((film) => id === film.id);
+  if (!film) {
+    res.status(404).json("A film with provided ID does not exist");
+  }
   res.json({ film });
 });
 
@@ -29,10 +28,28 @@ router.put("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  if (!req.body.title) {
+    res.status(400).json("Missing fields in request body");
+  }
+
+  const title = req.body.title;
+  const filmTitle = films.find((film) => film.title === title);
+  if (filmTitle) {
+    res.status(409).json("A film with the provided title already exist");
+  }
+
   let id = films.length;
   id++;
   const film = { ...req.body, id };
   films.push(film);
   res.status(201).json({ film: film });
+});
+
+router.get("/", (req, res) => {
+  const director = req.query.director;
+  if (director) {
+    const getFilms = films.filter((film) => film.director === director);
+    res.json(getFilms);
+  } else res.json({ films });
 });
 module.exports = router;
