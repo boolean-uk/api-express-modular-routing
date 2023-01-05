2188429420718121e70e7f3d9e89860bd6b7f9bc
Joel Watkins <eck087@yahoo.co.uk>
2023-01-05 16:41:18 +0000

    added error extensions for users

--- src/routers/users.js
+++ src/routers/users.js
@@ -15,6 +15,18 @@ router.post("/", (req, res) => {
     id = users[users.length - 1].id + 1;
   }
 
+  if (!req.body.email) {
+    res.status(400).send("Missing fields in request body");
+    return;
+  }
+
+  const alreadyExists = users.find((user) => user.email === req.body.email);
+
+  if (alreadyExists) {
+    res.status(409).send("A user with the provided email already exists");
+    return;
+  }
+
   const newUser = { ...req.body, id };
   users.push(newUser);
   res.status(201).json({ newUser });
@@ -24,12 +36,21 @@ router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((singleUser) => singleUser.id === id);
 
+  if (!user) {
+    res.status(404).send("A user with the provided ID does not exist");
+  }
+
   res.json({ user });
 });
 
 router.delete("/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((singleUser) => singleUser.id === id);
+
+  if (!user) {
+    res.status(404).send("A user with the provided ID does not exist");
+  }
+
   users.splice(users.indexOf(user), 1);
   res.json({ user });
 });
@@ -37,6 +58,23 @@ router.delete("/:id", (req, res) => {
 router.put("/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((singleUser) => singleUser.id === id);
+
+  if (!user) {
+    res.status(404).send("A user with the provided ID does not exist");
+  }
+
+  if (!req.body.email) {
+    res.status(400).send("Missing fields in request body");
+    return;
+  }
+
+  const alreadyExists = users.find((user) => user.email === req.body.email);
+
+  if (alreadyExists) {
+    res.status(409).send("A user with the provided email already exists");
+    return;
+  }
+
   user.email = req.body.email;
   res.json({ user });
 });
