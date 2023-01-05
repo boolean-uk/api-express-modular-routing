18da9166f14e16553ba28e9ef93a07d594b1d73a
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-01-05 16:51:42 +0000

    extention 2 done - films

--- src/routers/films.js
+++ src/routers/films.js
@@ -5,6 +5,13 @@ const { films } = require("../../data/index.js");
 let id = 0;
 
 router.get("/", (req, res) => {
+  const director = req.query.director;
+  const directorFilms = films.filter((film) => film.director === director);
+  if (director) {
+    res.json(directorFilms);
+    return;
+  }
+
   res.json(films);
 });
 
@@ -41,57 +48,55 @@ router.delete("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const title = req.body.title;
-    const newData = { id: id, ...req.body };
-    const filmIndex = films.findIndex((film) => film.id === id);
-  
-    if (title === undefined)
-      return res.status(400).json({
-        error: "Missing fields in the request body",
-      });
-  
-    if (filmIndex === -1)
-      return res.status(404).json({
-        error: "A film with the provided ID does not exist",
-      });
-  
-    const found = films.find((film) => film.title === title);
-    if (found !== undefined)
-      return res.status(409).json({
-        error: "A film with the provided email already exists",
-      });
-  
-    films[filmIndex] = newData;
-    res.json(newData);
-  });
+  const id = Number(req.params.id);
+  const title = req.body.title;
+  const newData = { id: id, ...req.body };
+  const filmIndex = films.findIndex((film) => film.id === id);
 
-  router.patch("/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const film = films.find(film => film.id === id)
-    Object.assign(film, req.body)
-  
-    if (req.body === undefined)
-      return res.status(400).json({
-        error: "Missing fields in the request body",
-      });
-  
-    if (film === undefined)
-      return res.status(404).json({
-        error: "A film with the provided ID does not exist",
-      });
-    
-    if (req.body.title !== undefined) {
+  if (title === undefined)
+    return res.status(400).json({
+      error: "Missing fields in the request body",
+    });
+
+  if (filmIndex === -1)
+    return res.status(404).json({
+      error: "A film with the provided ID does not exist",
+    });
+
+  const found = films.find((film) => film.title === title);
+  if (found !== undefined)
+    return res.status(409).json({
+      error: "A film with the provided email already exists",
+    });
+
+  films[filmIndex] = newData;
+  res.json(newData);
+});
+
+router.patch("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = films.find((film) => film.id === id);
+  Object.assign(film, req.body);
+
+  if (Object.keys(req.body).length === 0)
+    return res.status(400).json({
+      error: "Missing fields in the request body",
+    });
+
+  if (film === undefined)
+    return res.status(404).json({
+      error: "A film with the provided ID does not exist",
+    });
+
+  if (req.body.title !== undefined) {
     const found = films.find((film) => film.title === req.body.title);
     if (found !== undefined)
       return res.status(409).json({
         error: "A film with the provided title already exists",
       });
+  }
 
-    }
-  
-    res.json(film);
-  });
-
+  res.json(film);
+});
 
 module.exports = router;
