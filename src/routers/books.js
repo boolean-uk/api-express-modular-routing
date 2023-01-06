ccd57279b7f4a480e51087e05899f6d1d3d16c5e
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-06 11:04:35 +0000

    all tests now passing

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { books } = require("../../data/index.js");
+const { books } = require('../../data/index.js')
 
-router.get("/", (req, res) => {
-  res.json(books);
-});
+router.get('/', (req, res) => {
+  res.json({ books: books })
+})
 
-router.post("/", (req, res) => {
-  let id;
+router.post('/', (req, res) => {
+  let id
   if (books.length === 0) {
-    id = 1;
+    id = 1
   } else {
-    id = books[books.length - 1].id + 1;
+    id = books[books.length - 1].id + 1
   }
-  const newBook = { ...req.body, id };
-  books.push(newBook);
-  res.status(201).json({ newBook });
-});
+  const newBook = { ...req.body, id }
+  books.push(newBook)
+  res.status(201).json({ book: newBook })
+})
 
-router.get("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const book = films.find((singleBook) => singleBook.id === id);
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const book = books.find((singleBook) => singleBook.id === id)
 
-  res.json({ book });
-});
+  res.json({ book: book })
+})
 
-router.delete("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const book = books.find((singleBook) => singleBook.id === id);
-  books.splice(books.indexOf(book), 1);
-  res.json({ book });
-});
+router.delete('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const book = books.find((singleBook) => singleBook.id === id)
+  books.splice(books.indexOf(book), 1)
+  res.status(200).json({ book: book })
+})
 
-router.put("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const book = books.find((singleBook) => singleBook.id === id);
-  Object.keys(req.body).forEach((prop) => (book[prop] = req.body[prop]));
-  res.json({ book });
-});
+router.put('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const book = books.find((singleBook) => singleBook.id === id)
+  Object.keys(req.body).forEach((prop) => (book[prop] = req.body[prop]))
+  res.json({ book: book })
+})
 
-module.exports = router;
+module.exports = router
--- src/routers/films.js
+++ src/routers/films.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const { films } = require("../../data/index.js");
+const { films } = require('../../data/index.js')
 
-router.get("/", (req, res) => {
-  res.json(films);
-});
+router.get('/', (req, res) => {
+  res.json({ films: films })
+})
 
-router.post("/", (req, res) => {
-  let id;
+router.post('/', (req, res) => {
+  let id
   if (films.length === 0) {
-    id = 1;
+    id = 1
   } else {
-    id = films[films.length - 1].id + 1;
+    id = films[films.length - 1].id + 1
   }
-  const newFilm = { ...req.body, id };
-  films.push(newFilm);
-  res.status(201).json({ newFilm });
-});
+  const newFilm = { ...req.body, id }
+  films.push(newFilm)
+  res.status(201).json({ film: newFilm })
+})
 
-router.get("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const film = films.find((singleFilm) => singleFilm.id === id);
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const film = films.find((singleFilm) => singleFilm.id === id)
 
-  res.json({ film });
-});
+  res.json({ film: film })
+})
 
-router.delete("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const film = films.find((singleFilm) => singleFilm.id === id);
-  films.splice(films.indexOf(film), 1);
-  res.json({ film });
-});
+router.delete('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const film = films.find((singleFilm) => singleFilm.id === id)
+  films.splice(films.indexOf(film), 1)
+  res.json({ film: film })
+})
 
-router.put("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const film = films.find((singleFilm) => singleFilm.id === id);
-  Object.keys(req.body).forEach((prop) => (film[prop] = req.body[prop]));
-  res.json({ film });
-});
+router.put('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const film = films.find((singleFilm) => singleFilm.id === id)
+  Object.keys(req.body).forEach((prop) => (film[prop] = req.body[prop]))
+  res.json({ film: film })
+})
 
-module.exports = router;
+module.exports = router
