452064e45665e28d94cb6a18c1f7c685c47b45e6
Enky <enkh6at@yahoo.com>
2023-01-09 00:44:50 +0000

    complete

--- src/routers/films.js
+++ src/routers/films.js
@@ -10,14 +10,72 @@ router.get("/", (req, res) => {
 
 // create films
 router.post("/", (req, res) => {
-  let id;
-  if (films.length === 0) {
-    id = 1;
-  } else {
-    id = films[films.length - 1].id + 1;
-    console.log("hi", films.length);
-
+  if (!req.body.title || !req.body.director) {
+    res.status(400).json({ error: "Missing fields in request body" });
+    return;
   }
-  res.json(films)
+
+  const alreadyExists = films.find((film) => film.title === req.body.title);
+  if (alreadyExists) {
+    res
+      .status(409)
+      .json({ error: "A user with the provided email already exists" });
+  }
+
+  const film = { ...req.body, id: films.length + 1 };
+  films.push(film);
+  res.status(201).json({ film: film });
 });
+
+// get user by id
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = films.find((film) => film.id === id);
+
+  if (!film) {
+    res
+      .status(404)
+      .json({ error: "A user with the provided ID does not exist" });
+  }
+
+  res.json({ film: film });
+});
+
+// delete user by id
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = films.find((film) => film.id === id);
+
+  if (!film) {
+    res
+      .status(404)
+      .json({ error: "A user with the provided ID does not exist" });
+  }
+
+  films.splice(films.indexOf(film), 1);
+  res.status(201).json({ film: film });
+  //   console.log("hello",)
+});
+// edit user by id
+router.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = films.find((film) => film.id === id);
+  if (!film) {
+    res
+      .status(404)
+      .json({ error: "A user with the provided ID does not exist" });
+  }
+
+  const alreadyExists = films.find((film) => film.title === req.body.title);
+
+  if (alreadyExists) {
+    res
+      .status(409)
+      .json({ error: "A user with the provided email already exists" });
+  }
+  Object.keys(req.body).forEach((prop) => (film[prop] = req.body[prop]));
+  res.json({ film: film });
+});
+
+
 module.exports = router;
--- src/routers/users.js
+++ src/routers/users.js
@@ -5,32 +5,27 @@ const { users } = require("../../data/index.js");
 
 // get all user
 router.get("/", (req, res) => {
-  res.json(users);
+  res.json({ users: users });
 });
 
 // create user
 router.post("/", (req, res) => {
-  let id;
-  if (users.length === 0) {
-    id = 1;
-  } else {
-    id = users[users.length - 1].id + 1;
-  }
-
   if (!req.body.email) {
-    res.status(400).send("Missing fields in request body");
+    res.status(400).json({ error: "Missing fields in request body" });
     return;
   }
 
   const alreadyExists = users.find((user) => user.email === req.body.email);
   if (alreadyExists) {
-    res.status(409).send("A user with the provided email already exists");
+    res
+      .status(409)
+      .json({ error: "A user with the provided email already exists" });
     return;
   }
-  
-  const newUser = { ...req.body, id };
+
+  const newUser = { ...req.body, id: users.length + 1 };
   users.push(newUser);
-  res.status(201).json({ newUser });
+  res.status(201).json({ user: newUser });
 });
 
 // get user by id
@@ -39,10 +34,10 @@ router.get("/:id", (req, res) => {
   const user = users.find((singleUser) => singleUser.id === id);
 
   if (!user) {
-    res.status(404).send("A user with the provided ID does not exist");
+    res.status(404).json({ error: "A user with the provided ID does not exist" });
   }
 
-  res.json({ user });
+  res.json({ user: user });
 });
 
 // delete user by id
@@ -51,36 +46,30 @@ router.delete("/:id", (req, res) => {
   const user = users.find((singleUser) => singleUser.id === id);
 
   if (!user) {
-    res.status(404).send("A user with the provided ID does not exist");
+    res.status(404).json({ error: "A user with the provided ID does not exist" });
   }
 
   users.splice(users.indexOf(user), 1);
-  res.json({ user });
+  res.status(201).json({ user: user });
+//   console.log("hello",)
 });
-
 // edit user by id
 router.put("/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((singleUser) => singleUser.id === id);
   console.log(user);
   if (!user) {
-    res.status(404).send("A user with the provided ID does not exist");
-  }
-
-  if (!req.body.email) {
-    res.status(400).send("Missing fields in request body");
-    return;
+    res.status(404).json({ error: "A user with the provided ID does not exist" });
   }
 
   const alreadyExists = users.find((user) => user.email === req.body.email);
 
   if (alreadyExists) {
-    res.status(409).send("A user with the provided email already exists");
+    res.status(409).json({ error: "A user with the provided email already exists" });
     return;
   }
-
-  user.email = req.body.email;
-  res.json({ user });
+  Object.keys(req.body).forEach((prop) => (user[prop] = req.body[prop]));
+  res.status(201).json({ user: user });
 });
 
 module.exports = router;
