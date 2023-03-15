b048f0c8ca620ac08206cc9c6d858c5d64bf40f3
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-15 13:07:03 +0000

    All standard criteria endpoints implemented and tests passing

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,4 +1,42 @@
-// Import data here...
+// For the data, import the module from the file
+const { books } = require("../../data");
 
+// This enables the use of express router
+const router = require("express").Router();
 
-// Write routes here...
+// ROUTES
+
+router.get("/", (req, res) => {
+  res.json({ books });
+});
+
+router.get("/:id", (req, res) => {
+  const book = findBook(req.params.id);
+  res.json({ book });
+});
+
+router.post("/", (req, res) => {
+  const newId = books[books.length - 1].id + 1;
+  const book = { ...req.body, id: newId };
+  books.push(book);
+  res.status(201).json({ book });
+});
+
+router.put("/:id", (req, res) => {
+  const book = findBook(req.params.id);
+  const updatedBook = { id: book.id, ...req.body };
+  books[books.indexOf(book)] = updatedBook;
+  res.json({ book: updatedBook });
+});
+
+router.delete("/:id", (req, res) => {
+  const book = findBook(req.params.id);
+  books.splice(books.indexOf(book), 1);
+  res.json({ book });
+});
+
+function findBook(id) {
+  return books.find((book) => book.id == id);
+}
+
+module.exports = router;
--- test/api/routes/users.spec.js
+++ test/api/routes/users.spec.js
@@ -1,73 +1,72 @@
-const supertest = require("supertest")
-let app
-const { user1, user2 } = require("../../fixtures/userData.js")
+const supertest = require("supertest");
+let app;
+const { user1, user2 } = require("../../fixtures/userData.js");
 
 describe("Users Endpoint", () => {
   beforeEach(() => {
-    app = require("../../../src/server.js")
-  })
+    app = require("../../../src/server.js");
+  });
   describe("GET /users", () => {
     it("will list all users", async () => {
-      const response = await supertest(app).get("/users")
+      const response = await supertest(app).get("/users");
 
-      expect(response.status).toEqual(200)
-      expect(response.body.users).not.toEqual(undefined)
-      expect(response.body.users.length).toEqual(3)
-    })
+      expect(response.status).toEqual(200);
+      expect(response.body.users).not.toEqual(undefined);
+      expect(response.body.users.length).toEqual(3);
+    });
 
     it("will list a user", async () => {
-      const response = await supertest(app).get("/users/1")
+      const response = await supertest(app).get("/users/1");
 
-      expect(response.status).toEqual(200)
-      expect(response.body.user).not.toEqual(undefined)
-      expect(response.body.user.id).toEqual(1)
-      expect(response.body.user.email).toEqual("edward@mail.com")
-    })
-  })
+      expect(response.status).toEqual(200);
+      expect(response.body.user).not.toEqual(undefined);
+      expect(response.body.user.id).toEqual(1);
+      expect(response.body.user.email).toEqual("edward@mail.com");
+    });
+  });
 
   describe("POST /users", () => {
-
     it("will create a new user", async () => {
-      const response = await supertest(app).post("/users").send(user1)
+      const response = await supertest(app).post("/users").send(user1);
 
-      expect(response.status).toEqual(201)
-      expect(response.body.user).not.toEqual(undefined)
-      expect(response.body.user.id).toEqual(4)
+      expect(response.status).toEqual(201);
+      expect(response.body.user).not.toEqual(undefined);
+      expect(response.body.user.id).toEqual(4);
 
       for (prop in user1) {
-        expect(response.body.user[prop]).toEqual(user1[prop])
+        expect(response.body.user[prop]).toEqual(user1[prop]);
       }
-    })
-  })
+    });
+  });
 
   describe("PUT /users", () => {
     it("will update a users", async () => {
-      const response = await supertest(app).put("/users/1").send(user2)
+      const response = await supertest(app).put("/users/1").send(user2);
 
-      expect(response.status).toEqual(201)
-      expect(response.body.user).not.toEqual(undefined)
-      expect(response.body.user.id).toEqual(1)
-      expect(response.body.user.email).toEqual("updateaddress@mail.com")
-    })
-  })
+      expect(response.status).toEqual(201);
+      expect(response.body.user).not.toEqual(undefined);
+      expect(response.body.user.id).toEqual(1);
+      expect(response.body.user.email).toEqual("updateaddress@mail.com");
+    });
+  });
 
   describe("DELETE /users", () => {
     it("will return the deleted the user", async () => {
-      const response = await supertest(app).delete("/users/1")
+      const response = await supertest(app).delete("/users/1");
 
-      expect(response.status).toEqual(201)
-      expect(response.body.user).not.toEqual(undefined)
-      expect(response.body.user.id).toEqual(1)
-      expect(response.body.user.email).toEqual("edward@mail.com")
-    })
+      expect(response.status).toEqual(201);
+      expect(response.body.user).not.toEqual(undefined);
+      expect(response.body.user.id).toEqual(1);
+      expect(response.body.user.email).toEqual("edward@mail.com");
+    });
 
     it("will remove the user", async () => {
-      const responseBefore = await supertest(app).get("/users")
-      await supertest(app).delete("/users/1")
-      const responseAfter = await supertest(app).get("/users")
+      const responseBefore = await supertest(app).get("/users");
+      await supertest(app).delete("/users/1");
+      const responseAfter = await supertest(app).get("/users");
 
-      expect(responseBefore.body.users.length).toEqual(3)
-      expect(responseAfter.body.users.length).toEqual(2)
-    })
-  })
-})
+      expect(responseBefore.body.users.length).toEqual(3);
+      expect(responseAfter.body.users.length).toEqual(2);
+    });
+  });
+});
