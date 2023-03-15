a78aa491e21eec967a747884978ce29a34260b29
benji <benjipictor@gmail.com>
2023-03-15 15:14:39 +0000

    extensions complete

--- src/routers/users.js
+++ src/routers/users.js
@@ -33,17 +33,26 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const user = users.find((obj) => obj.id === id)
-    Object.keys(req.body).forEach((prop) => user[prop] = req.body[prop])
-    res.status(201).json({user})
+    const user = users.find((obj) => obj.id === Number(req.params.id))
+    const found = users.find((obj) => obj.email === req.body.email)
+    if(user === undefined) {
+        res.status(404).json({ error: 'A user with the provided ID does not exist'})
+    }  else if(found !== undefined) {
+        res.status(409).json({ error: 'A user with the provided email already exists'})
+    } else {
+        Object.keys(req.body).forEach((prop) => user[prop] = req.body[prop])
+        res.status(200).json({user})
+    }
 })
 router.delete('/:id', (req, res) => {
-    const user = users.find((obj) => obj.id === Number(req.params.id) )
-    const index = users.indexOf(user)
-    users.splice(index, 1)
-    res.status(201).json({user})
-
+    const user = users.find((obj) => obj.id === Number(req.params.id))
+    if(user === undefined) {
+        res.status(404).json({ error: 'A user with the provided ID does not exist'})
+    } else {
+        const index = users.indexOf(user)
+        users.splice(index, 1)
+        res.status(200).json({user})
+    }
 })
 
 module.exports = router
