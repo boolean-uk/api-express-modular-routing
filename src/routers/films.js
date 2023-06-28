38068224b84587b94e18bf82a6928e224d1407bc
Mantas Navickis <navickis@gmail.com>
2023-06-28 19:25:24 +0200

    Core and ext done

--- src/routers/films.js
+++ src/routers/films.js
@@ -28,34 +28,67 @@ const findById = (id) => {
   return films.find((film) => film.id === id)
 }
 router.get('/', (req, res) => {
-  res.send({ films })
+  if (!req.query.director) {
+    res.send({ films })
+  } else {
+    const director = req.query.director
+    director.split('_').join(' ')
+    const filteredArray = films.filter((film) => film.director === director)
+    res.send({ films: filteredArray })
+  }
 })
 router.post('/', (req, res) => {
   const body = req.body
-  body.id = id++
-  films.push(body)
-  res.status(201).send({ film: body })
+  if (!Object.keys(body)[0]) {
+    console.log(Object.keys(body))
+    res.status(400).send({ error: 'Missing fields in request body' })
+  } else if (films.find((film) => film.director === body.director)) {
+    res
+      .status(409)
+      .send({ error: 'A film with the provided title already exists' })
+  } else {
+    body.id = id++
+    films.push(body)
+    res.status(201).send({ film: body })
+  }
 })
 router.get('/:id', (req, res) => {
   const idNum = Number(req.params.id)
   const film = findById(idNum)
-  res.send({ film })
+  if (!film) {
+    res.status(404).send({ error: 'A film with provided ID does not exist' })
+  } else {
+    res.send({ film })
+  }
 })
 router.delete('/:id', (req, res) => {
   const idNum = Number(req.params.id)
   const filmIndex = films.findIndex((film) => film.id === idNum)
   const film = findById(idNum)
-  films.splice(filmIndex, 1)
-  res.send({ film })
+  if (!film) {
+    res.status(404).send({ error: 'A film with provided ID does not exist' })
+  } else {
+    films.splice(filmIndex, 1)
+    res.send({ film })
+  }
 })
 router.put('/:id', (req, res) => {
   const idNum = Number(req.params.id)
   const body = req.body
   const filmIndex = films.findIndex((film) => film.id === idNum)
   const film = findById(idNum)
-  films[filmIndex] = body
-  films[filmIndex].id = film.id
-  res.send({ film: films[filmIndex] })
+
+  if (!film) {
+    res.status(404).send({ error: 'A film with provided ID does not exist' })
+  } else if (films.find((film) => film.title === body.title)) {
+    res
+      .status(409)
+      .send({ error: 'A film with the provided title already exists' })
+  } else {
+    films[filmIndex] = body
+    films[filmIndex].id = film.id
+    res.send({ film: films[filmIndex] })
+  }
 })
 router.patch('/:id', (req, res) => {
   const idNum = Number(req.params.id)
@@ -77,15 +110,6 @@ router.patch('/:id', (req, res) => {
       res.status(200).send({ film: filmKeys })
     })
   }
-})
-router.get('/', (req, res) => {
-  const director = req.query
-  console.log(director)
-  director.split('_').join(' ')
-  console.log(director)
-  const filteredArray = films.filter((film) => film.director === director)
-  console.log(filteredArray)
-  res.send({ films: filteredArray })
 })
 
 module.exports = router
