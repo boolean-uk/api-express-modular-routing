d5ecd7f08c40ea3f0437441b346e037c72010ed6
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-28 16:11:28 +0200

    working on ext books

--- src/routers/films.js
+++ src/routers/films.js
@@ -14,14 +14,31 @@ router.post('/', (req, res) => {
       ...req.body,
       id: newId
     }
+    
+  const title = films.find((film) => film.title === newFilm.title);
+
+  if(title) {
+    return res
+    .status(409)
+    .send({ error: "A film with the provided title already exists" });
+  }
+
+ if (newFilm.title !== "" && newFilm.title !== undefined) {
     films.push(newFilm)
     return res.status(201).send({film: newFilm})
+  }
+  return res.status(400).send({ error: "Missing fields in request body" });
+
   })
 
 router.get('/:id', (req, res) => {
   const id = Number(req.params.id)  
   const film = films.find((film) => film.id === id)
-
+  if (!film) {
+    return res
+      .status(404)
+      .send({ error: "A film with provided ID does not exist" });
+  }
   return res.status(200).send({ film: film})
 })
 
@@ -29,21 +46,43 @@ router.delete('/:id', (req, res) => {
   const id = Number(req.params.id)  
   const film = films.find((film) => film.id === id)
 
-  const filmIndex = films.indexOf(film)
-  films.splice(filmIndex, 1)
+  if(film) {
+    const filmIndex = films.indexOf(film)
+    films.splice(filmIndex, 1)
+  
+    return res.send({film: film})
+  }
+  else {
+    return res
+      .status(404)
+      .send({ error: "A film with provided ID does not exist" });
+  }
 
-  return res.send({film: film})
 })
 
 router.put('/:id', (req, res) => {
   const id = Number(req.params.id)
   const updatedFilm = req.body
   const filmToUpdate = films.find((film) => film.id === id)
+  if(filmToUpdate) {
+    Object.assign(filmToUpdate, updatedFilm)
+    const title = films.find((film) => film.title === updatedFilm.title);
 
-  Object.assign(filmToUpdate, updatedFilm)
-
-  const newFilm = films.find((film) => film.id === id)
-  return res.send({film: newFilm})
+    if(title) {
+      return res
+      .status(409)
+      .send({ error: "A film with the provided title already exists" });
+    }
+    const newFilm = films.find((film) => film.id === id)
+    return res.send({film: newFilm})
+  }
+  
+  else {
+    return res
+      .status(404)
+      .send({ error: "A film with provided ID does not exist" });
+  }
+  
 })
 
 module.exports = router;
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,47 +1,78 @@
 const express = require("express");
-const router = express.Router()
-let { users } = require('../../data/index')
+const router = express.Router();
+let { users } = require("../../data/index");
 
-let newId = users.length
+let newId = users.length;
 
-router.get('/', (req, res) => {
-  return res.send({ users })
-})
+router.get("/", (req, res) => {
+  return res.send({ users });
+});
 
-router.post('/', (req, res) => {
-  newId++
+router.post("/", (req, res) => {
+  newId++;
   const newUser = {
     ...req.body,
-    id: newId
+    id: newId,
+  };
+
+  const email = users.find((user) => user.email === newUser.email);
+
+  if (email) {
+    return res
+      .status(409)
+      .send({ error: "A user with the provided email already exists" });
   }
-  users.push(newUser)
-  return res.status(201).send({user: newUser})
-})
-
-router.get('/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const user = users.find((user) => user.id === id)
-  return res.send({ user: user })
-})
-
-router.delete('/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const userDel = users.find((user) => user.id === id)
-  if(userDel) {
-    users = users.filter((user) => user.id !== id)
-    return res.send({user: userDel})
+ if (newUser.email !== "" && newUser.email !== undefined) {
+    users.push(newUser);
+    return res.status(201).send({ user: newUser });
   }
 
-})
+  return res.status(400).send({ error: "Missing fields in request body" });
+});
 
-router.put('/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const userToUpdate = users.find((user) => user.id === id)
-
-  if(userToUpdate) {
-    userToUpdate.email = req.body.email
-    return res.send({user: userToUpdate})
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  if (!user) {
+    return res
+      .status(404)
+      .send({ error: "A user with the provided ID does not exist" });
   }
-})
+  return res.send({ user: user });
+});
 
-module.exports = router
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const userDel = users.find((user) => user.id === id);
+  if (userDel) {
+    users = users.filter((user) => user.id !== id);
+    return res.send({ user: userDel });
+  } else {
+    return res
+      .status(404)
+      .send({ error: "A user with the provided ID does not exist" });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const userToUpdate = users.find((user) => user.id === id);
+
+  if (userToUpdate) {
+    userToUpdate.email = req.body.email;
+    const email = users.find((user) => user.email === userToUpdate.email);
+
+    if (email) {
+      return res
+        .status(409)
+        .send({ error: "A user with the provided email already exists" });
+    }
+    return res.send({ user: userToUpdate });
+  } else {
+    return res
+      .status(404)
+      .send({ error: "A user with the provided ID does not exist" });
+  }
+});
+
+module.exports = router;
