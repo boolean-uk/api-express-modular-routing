9c338ca7a9876979013a42feafaf8ef25cc714b3
Todd Griffin <todd.griffin61@outlook.com>
2023-06-28 12:56:26 +0100

    films core done

--- src/routers/films.js
+++ src/routers/films.js
@@ -18,4 +18,32 @@ router.post('/', (req, res) => {
     return res.status(201).send({film: newFilm})
   })
 
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)  
+  const film = films.find((film) => film.id === id)
+
+  return res.status(200).send({ film: film})
+})
+
+router.delete('/:id', (req, res) => {
+  const id = Number(req.params.id)  
+  const film = films.find((film) => film.id === id)
+
+  const filmIndex = films.indexOf(film)
+  films.splice(filmIndex, 1)
+
+  return res.send({film: film})
+})
+
+router.put('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const updatedFilm = req.body
+  const filmToUpdate = films.find((film) => film.id === id)
+
+  Object.assign(filmToUpdate, updatedFilm)
+
+  const newFilm = films.find((film) => film.id === id)
+  return res.send({film: newFilm})
+})
+
 module.exports = router;
