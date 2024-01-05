70658e8fba5688558f4180e2bd67676be97433d9
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-05 11:53:13 +0000

    feat: retrieve all users

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const data = require("../../data/index")
+const users = data.users
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    return res.json({"users": users})
+})
 
 module.exports = router
