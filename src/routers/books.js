8d756bca36b38aa990d2c4c4688b0b1351762490
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-05 19:41:51 +0000

    retrieve and test passed for books

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,5 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 // Write routes here...
+const { books } = require('../../data/index.js');
+
+const findBook = (req, res) => {
+    const bookId = Number(req.params.id);
+    const foundBook = books.find((book) => book.id === bookId);
+    if (!foundBook) {
+        res
+          .status(404)
+          .json({ message: `Book with the ID ${bookId} does not exist!` });
+    }
+    return foundBook;
+};
+
+router.get('/', (req, res) => {
+    res.status(200).json({ books });
+});
+
 
 module.exports = router;
