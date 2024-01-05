e4ebc24ba96643f2c3d7c5deb6f8dfbe9eb734c7
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 14:35:07 +0000

    Get a single user by ID - Test passed

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
-const users = require('../../data/index.js')
+const data = require('../../data/index.js')
+
+const findUser = (req, res) => {
+    const userId = Number(req.params.id)
+
+    const findUser = data.users.find(user => user.id === userId)
+
+    if (!findUser) {
+        res.status(404).json({message: `no such user with id: ${userId}`})
+    }
+
+    return findUser
+}
 
 router.get('/', (re, res) => {
-    return res.status(200).json({users: users.users})
+    return res.status(200).json({users: data.users})
 })
+
+router.get('/:id', (req, res) => {
+    const user = findUser(req, res)
+
+    return res.status(200).json({user})
+})
+
+
  
 module.exports = router
