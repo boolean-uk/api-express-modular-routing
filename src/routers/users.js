4864249f4cfd86f4fea618f8d5ef98811528df69
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 15:07:41 +0000

    Create a user - Test passed

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const data = require("../../data/index.js");
+const {user1, user2} = require('../../test/fixtures/userData.js')
+
+let currentId = 3
 
 const findUser = (req, res) => {
   const userId = Number(req.params.id);
@@ -26,5 +29,22 @@ router.get("/:id", (req, res) => {
 
   return res.status(200).json({ user });
 });
+
+
+router.post('/', (req, res) => {
+    const body = user1;
+
+    const newUser = {
+      id: ++currentId,
+      ...body,
+    };
+  
+    data.users.push({ ...newUser });
+  
+    // 'Missing fields in request body'
+
+    res.status(201).json({ user: newUser });
+})
+
 
 module.exports = router;
