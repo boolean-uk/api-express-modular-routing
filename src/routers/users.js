9d6f4422e95abe3adeaaace48b054d346bd8db81
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-05 18:44:36 +0000

    find user by Id and test passed

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const users = require("../../data/index.js")
+const data = require("../../data/index.js");
 
-router.get('/', (req, res) => {
-    res.status(200).json({users: users.users})
-})
+const users = data.users;
+
+const findUser = (req, res) => {
+  const userId = Number(req.params.id);
+
+  const foundUser = users.find((user) => user.id === userId);
+
+  if (!foundUser) {
+    res
+      .status(404)
+      .json({ message: `User with the ID ${userId} does not exist!` });
+  }
+  return foundUser;
+};
+
+router.get("/", (req, res) => {
+  res.status(200).json({ users: users });
+});
+
+router.get("/:id", (req, res) => {
+    const foundUserById = findUser(req, res);
+    res.status(200).json({ user: foundUserById });
+  });
 module.exports = router;
