0d9304223bac5c1eb9ad23327b328e6b7b1d6b82
callumhayden <callum.hayden@gmail.com>
2024-01-05 12:08:01 +0000

    did get request to receive all the books

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,6 +1,13 @@
 // Import data here...
 const express = require('express')
+const {books} = require("../../data/index")
 const router = express.Router()
+
+
+// Get request for all films 
+router.get("/", (req, res) => {
+res.status(200).json({books: books})
+})
 
 // Write routes here...
 module.exports = router
