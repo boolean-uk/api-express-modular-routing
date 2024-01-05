e0adc7dab4cae20eb3f5ef083d6050cf36342967
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 14:19:42 +0000

    List all users - test passed

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const users = require('../../data/index.js')
 
-
+router.get('/', (re, res) => {
+    return res.status(200).json({users: users.users})
+})
  
 module.exports = router
