72de17915ffc11c93b8a531c219a3b9ec73fcf07
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-05 12:36:49 +0000

    feat: add nextId var

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const {findNextId} = require("../utilities.js")
+
 const { users: userData } = require("../../data/index.js");
+let nextId = findNextId(userData)
 
 router.get("/", (req, res) => {
   return res.json({ users: userData });
