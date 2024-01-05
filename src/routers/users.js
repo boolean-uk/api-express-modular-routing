408d7886bf6e58d01f42ed669e24a75f8ff57fd3
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-05 12:44:02 +0000

    style: prettier format

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const {findNextId} = require("../utilities.js")
+const { findNextId } = require("../utilities.js");
 
 const { users: userData } = require("../../data/index.js");
-let nextId = findNextId(userData)
+let nextId = findNextId(userData);
 
 router.get("/", (req, res) => {
   return res.json({ users: userData });
 });
 
 router.post("/", (req, res) => {
-  const { email } = req.body
-  const newUser = {id: nextId++, email}
+  const { email } = req.body;
+  const newUser = { id: nextId++, email };
 
-  userData.push(newUser)
-  res.status(201).json({user: newUser})
-})
+  userData.push(newUser);
+  res.status(201).json({ user: newUser });
+});
 
 module.exports = router;
