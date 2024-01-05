7a31989c22943e7a5b44629998fe6a6bb3f5daf5
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 17:01:59 +0000

    Gett all lists -  test pased

--- src/routers/books.js
+++ src/routers/books.js
@@ -2,6 +2,26 @@
 const express = require('express')
 const router = express.Router()
 
+const data = require('../../data/index.js')
+const {book1, book2, book3} = require('../../test/fixtures/bookData.js')
+
+const findBook = (req, res) => {
+    const bookId = Number(req.params.id);
+  
+    const foundBook = data.books.find((book) => book.id === bookId);
+  
+    if (!foundBook) {
+      res
+        .status(404)
+        .json({ error: "A book with the provided ID does not exist" });
+    }
+  
+    return foundBook;
+  };
+
 // Write routes here...
+router.get('/', (req, res) => {
+    return res.json({books: data.books})
+})
 
 module.exports = router
