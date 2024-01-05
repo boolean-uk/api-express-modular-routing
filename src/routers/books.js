6d69b47fd66ba578055da7d0618ab9846a66eb30
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-05 16:26:43 +0000

    feat: add a new book

--- src/routers/books.js
+++ src/routers/books.js
@@ -5,4 +5,14 @@ const books = data.books
 
 router.get("/", (req, res) => (res.json({books: books})))
 
+router.post("/", (req, res) => {
+    const id = books.length + 1
+    const newBook = req.body
+    newBook.id = id
+
+    books.push(newBook)
+
+    return res.status(201).json({book: newBook})
+})
+
 module.exports = router
