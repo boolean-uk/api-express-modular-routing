46c9b7573c8a9e53bfa0d563285f762bbd6c11b2
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-05 12:15:07 +0000

    feat: add get /users

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,4 +3,8 @@ const router = express.Router();
 
 const { users: userData } = require("../../data/index.js");
 
+router.get("/", (req, res) => {
+  return res.json({ users: userData });
+});
+
 module.exports = router;
