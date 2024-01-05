ff4a07a1dbce886f25e4298d8371de71f569045c
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 15:25:12 +0000

    Extension - return 400 for missing fields when creating a user and return 409 if new user's email exists in users list

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const data = require("../../data/index.js");
-const {user1, user2} = require('../../test/fixtures/userData.js')
+const { user1, user2 } = require("../../test/fixtures/userData.js");
 
-let currentId = 3
+let currentId = 3;
 
 const findUser = (req, res) => {
   const userId = Number(req.params.id);
@@ -30,21 +30,28 @@ router.get("/:id", (req, res) => {
   return res.status(200).json({ user });
 });
 
+router.post("/", (req, res) => {
+  const body = user1;
 
-router.post('/', (req, res) => {
-    const body = user1;
+  const newUser = {
+    id: ++currentId,
+    ...body,
+  };
 
-    const newUser = {
-      id: ++currentId,
-      ...body,
-    };
-  
-    data.users.push({ ...newUser });
-  
-    // 'Missing fields in request body'
+  data.users.push({ ...newUser });
 
-    res.status(201).json({ user: newUser });
-})
+  if (body.length === 0) {
+    res.status(400).json({ error: "Missing fields in request body" });
+  }
+  for (let i = 0; i < data.users.length; i++) {
+    if (body === data.users[i].email) {
+      res
+        .status(409)
+        .json({ error: "A user with the provided email already exists" });
+    }
+  }
 
+  res.status(201).json({ user: newUser });
+});
 
 module.exports = router;
