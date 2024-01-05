37b53e00a76d1ac67f6a58eb48471528a227c5af
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-05 19:38:21 +0000

    delete and test passed for film by Id

--- src/routers/films.js
+++ src/routers/films.js
@@ -1,38 +1,43 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {films} = require('../../data/index.js')
+const { films } = require("../../data/index.js");
 
 const findFilm = (req, res) => {
-    const filmId = Number(req.params.id)
-    const foundFilm = films.find(film => film.id === filmId)
-    if (!foundFilm) {
-      res
+  const filmId = Number(req.params.id);
+  const foundFilm = films.find((film) => film.id === filmId);
+  if (!foundFilm) {
+    res
       .status(404)
-      .json({ message: `Film with the ID ${filmId} does not exist!` })
-    }
-    return foundFilm
-}
+      .json({ message: `Film with the ID ${filmId} does not exist!` });
+  }
+  return foundFilm;
+};
 
-router.get('/', (req, res) => {
-  res.status(200).json({films})
+router.get("/", (req, res) => {
+  res.status(200).json({ films });
+});
+
+router.get("/:id", (req, res) => {
+  const foundFilm = findFilm(req, res);
+  res.status(200).json({ film: foundFilm });
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+  const newFilm = {
+    id: films.length + 1,
+    title: body.title,
+    director: body.director,
+  };
+  films.push(newFilm);
+  res.status(201).json({ film: newFilm });
+});
+
+router.delete("/:id", (req, res) => {
+    const filmDelete = findFilm(req, res);
+    films.splice(films.indexOf(filmDelete), 1);
+    res.status(200).json({ message: `Film with the Id: ${filmDelete.id} and title: ${filmDelete.title}. Has been deleted`,  });
 })
 
-router.get('/:id', (req, res) => {
-  const foundFilm = findFilm(req, res)
-  res.status(200).json({film: foundFilm})
-})
-
-router.post('/', (req, res) => {
-    const body = req.body
-    const newFilm = {
-        id: films.length + 1,
-        title: body.title,
-        director: body.director
-    }
-    films.push(newFilm)
-    res.status(201).json({ film: newFilm })
-}
-)
-
-module.exports = router
+module.exports = router;
