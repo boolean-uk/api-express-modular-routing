b6f21b1d2a2190560633fee5162cdbe9ad34ce88
callumhayden <callum.hayden@gmail.com>
2024-01-05 12:21:21 +0000

    get request to get film by id

--- src/routers/films.js
+++ src/routers/films.js
@@ -9,5 +9,17 @@ router.get("/", (req, res) => {
 res.status(200).json({films: films})
 })
 
+// Get request for a single id
+router.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const film = films.find((film) => film.id === id)
+
+    if(film){
+        res.status(200).json({film: film})
+    } else {
+        res.status(404).json({error: "No such film with this id"})
+    }
+})
+
 // Write routes here...
 module.exports = router
