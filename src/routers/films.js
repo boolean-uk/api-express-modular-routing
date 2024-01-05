b28712e233aebf3c4e04da7e3e29727626020754
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-05 19:29:08 +0000

    retrieve, display and test pass for film by id

--- src/routers/films.js
+++ src/routers/films.js
@@ -3,8 +3,24 @@ const router = express.Router()
 
 const {films} = require('../../data/index.js')
 
+const findFilm = (req, res) => {
+    const filmId = Number(req.params.id)
+    const foundFilm = films.find(film => film.id === filmId)
+    if (!foundFilm) {
+      res
+      .status(404)
+      .json({ message: `Film with the ID ${filmId} does not exist!` })
+    }
+    return foundFilm
+}
+
 router.get('/', (req, res) => {
   res.status(200).json({films})
+})
+
+router.get('/:id', (req, res) => {
+  const foundFilm = findFilm(req, res)
+  res.status(200).json({film: foundFilm})
 })
 
 module.exports = router
