9f6a9231d2eea816fad0c3e3029927ecbfa635df
Lukas Dembicki <lad_777@hotmail.com>
2024-01-05 12:26:20 +0000

    ADD - GET: Retrieves all users.

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,6 +3,12 @@ const { users } = require('../../data/index.js')
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    return res.status(200).json({ users: users })
+})
+
+
+
 
 
 
