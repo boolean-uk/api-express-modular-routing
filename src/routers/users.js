752d46b10799d56384d2c4417c9ad784875250b6
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-05 17:11:26 +0100

    add  error handling for post user

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,6 +3,8 @@ const { users } = require('../../data/index')
 let userId = 0
 const getNewUserId = () => ++userId
 
+const duplicate = (email) => !!users.find((user) => user.email === email)
+
 class User {
   constructor(email){
     this.id = getNewUserId()
@@ -17,9 +19,20 @@ router.get('/', (req, res) => res.json({ users }))
 
 router.post('/', (req, res) => {
   const { email } = req.body
+
+  if (!email) {
+    res.status(400).json( { "error": "Missing fields in request body"})
+  }
+
+  if (duplicate) {
+    res.status(409).json({ "error": "A user with the provded email already exists" })
+  }
+
   const user = new User(email)
-  users.push(user)
-  res.status(201).json({ user })
+  if (user) {
+    users.push(user)
+    res.status(201).json({ user })
+  }
 })
 
 module.exports = router
