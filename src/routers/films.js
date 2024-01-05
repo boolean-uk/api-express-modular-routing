f75bb2bd2de66934c0a0eb9e40e7adfe1a9b5e33
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-05 13:20:18 +0000

    feat: add a new film

--- src/routers/films.js
+++ src/routers/films.js
@@ -3,6 +3,13 @@ const router = express.Router()
 const data = require('../../data/index')
 const films = data.films
 
-router.get("/", ((req, res) => (res.json({"films": films}))))
+router.get("/", (req, res) => (res.json({"films": films})))
 
+router.post("/", (req, res) => {
+    const id = films.length + 1
+    const newFilm = req.body
+    newFilm.id = id
+    films.push(newFilm)
+    return res.status(201).json({"film": newFilm})
+} )
 module.exports = router
