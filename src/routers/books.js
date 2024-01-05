c698b0bcc789f109413f6c7fac6fbba66ed73653
Lukas Dembicki <lad_777@hotmail.com>
2024-01-05 17:19:11 +0000

    ADD: POST '/' (Creates a new book and adds to list of books, checks for same titles.)

--- src/routers/books.js
+++ src/routers/books.js
@@ -5,8 +5,37 @@ const { books } = require('../../data/index.js')
 const express = require('express')
 const router = express.Router()
 
+let bookCounter = 4
+
+function bookMatch(newBook) {
+    const foundBook = books.find((book) => book.title === newBook.title);
+    if (foundBook) return true;
+    return false;
+}
+
 router.get("/", (req, res) => {
     return res.status(200).json({ books: books });
+});
+
+router.post("/", (req, res) => {
+    let newBook = req.body;
+
+    if (!newBook.title) {
+        return res
+            .status(400)
+            .json({ ERROR: "Missing fields in request body" });
+    }
+
+    if (bookMatch(newBook)) {
+        return res
+            .status(409)
+            .json({ ERROR: "A book with the provided title already exists" });
+    }
+
+    newBook = { id: ++bookCounter, ...newBook };
+    books.push(newBook);
+
+    return res.status(201).json({ book: newBook });
 });
 
 
