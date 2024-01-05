be6ea82d3390bc5668b24ee557651f9894ec9be3
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 14:44:25 +0000

    Extension - error: No such user with ID - 404 response

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const data = require('../../data/index.js')
+const data = require("../../data/index.js");
 
 const findUser = (req, res) => {
-    const userId = Number(req.params.id)
+  const userId = Number(req.params.id);
 
-    const findUser = data.users.find(user => user.id === userId)
+  const findUser = data.users.find((user) => user.id === userId);
 
-    if (!findUser) {
-        res.status(404).json({message: `no such user with id: ${userId}`})
-    }
+  if (!findUser) {
+    res
+      .status(404)
+      .json({ error: "A user with the provided ID does not exist" });
+  }
 
-    return findUser
-}
+  return findUser;
+};
 
-router.get('/', (re, res) => {
-    return res.status(200).json({users: data.users})
-})
+router.get("/", (re, res) => {
+  return res.status(200).json({ users: data.users });
+});
 
-router.get('/:id', (req, res) => {
-    const user = findUser(req, res)
+router.get("/:id", (req, res) => {
+  const user = findUser(req, res);
 
-    return res.status(200).json({user})
-})
+  return res.status(200).json({ user });
+});
 
-
- 
-module.exports = router
+module.exports = router;
