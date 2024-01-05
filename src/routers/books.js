6c2e35a0ada1d1f3e8d517624b729854f9280a42
callumhayden <callum.hayden@gmail.com>
2024-01-05 12:26:23 +0000

    get request to receive book by id

--- src/routers/books.js
+++ src/routers/books.js
@@ -9,5 +9,17 @@ router.get("/", (req, res) => {
 res.status(200).json({books: books})
 })
 
+// Get request for a single id
+router.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = books.find((book) => book.id === id)
+
+    if(book){
+        res.status(200).json({book: book})
+    } else {
+        res.status(404).json({error: "No such book with this id"})
+    }
+})
+
 // Write routes here...
 module.exports = router
--- src/routers/users.js
+++ src/routers/users.js
@@ -9,5 +9,17 @@ router.get("/", (req, res) => {
 res.status(200).json({users: users})
 })
 
+// Get request for a single id
+router.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find((user) => user.id === id)
+
+    if(user){
+        res.status(200).json({user: user})
+    } else {
+        res.status(404).json({error: "No such user with this id"})
+    }
+})
+
 // Write routes here...
 module.exports = router
