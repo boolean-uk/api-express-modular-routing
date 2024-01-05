cc29910ba4062d7d0c43ab93ab3e06060c2a28c6
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-05 13:11:34 +0000

    feat: add get films/

--- src/routers/films.js
+++ src/routers/films.js
@@ -6,4 +6,8 @@ const { findById, findNextId } = require("../utilities.js");
 const { films: filmData } = require("../../data/index.js");
 let nextId = findNextId(filmData);
 
+router.get("/", (req, res) => {
+  return res.json({ films: filmData });
+});
+
 module.exports = router
