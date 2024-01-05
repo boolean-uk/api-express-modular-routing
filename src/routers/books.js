105d175cac8954802e44f6e3b3e9410307f866a5
ilham saleh <ilham2saleh@gmail.com>
2024-01-05 17:11:23 +0000

    Delete a book by ID - core criterias for all tests complete

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,50 +1,69 @@
 // Import data here...
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const data = require('../../data/index.js')
-const {book1, book2, book3} = require('../../test/fixtures/bookData.js')
+const data = require("../../data/index.js");
+const { book1, book2, book3 } = require("../../test/fixtures/bookData.js");
+const { route } = require("./users");
 
 const findBook = (req, res) => {
-    const bookId = Number(req.params.id);
-  
-    const foundBook = data.books.find((book) => book.id === bookId);
-  
-    if (!foundBook) {
-      res
-        .status(404)
-        .json({ error: "A book with the provided ID does not exist" });
-    }
-  
-    return foundBook;
-  };
+  const bookId = Number(req.params.id);
 
-  let currentId = 4
+  const foundBook = data.books.find((book) => book.id === bookId);
+
+  if (!foundBook) {
+    res
+      .status(404)
+      .json({ error: "A book with the provided ID does not exist" });
+  }
+
+  return foundBook;
+};
+
+let currentId = 4;
 
 // Write routes here...
-router.get('/', (req, res) => {
-    return res.json({books: data.books})
-})
+router.get("/", (req, res) => {
+  return res.json({ books: data.books });
+});
 
-router.get('/:id', (req, res) => {
-    const book = findBook(req, res) 
+router.get("/:id", (req, res) => {
+  const book = findBook(req, res);
 
-    return res.json({book})
-})
+  return res.json({ book });
+});
 
-router.post('/', (req, res) => {
-    const body = book1
+router.post("/", (req, res) => {
+  const body = book1;
 
-    const newBook = {
-        id: ++currentId,
-        ...body
-    }
+  const newBook = {
+    id: ++currentId,
+    ...body,
+  };
 
-    data.books.push(newBook)
+  data.books.push(newBook);
 
-    return res.status(201).json({book: newBook})
-})
+  return res.status(201).json({ book: newBook });
+});
 
+router.put("/:id", (req, res) => {
+  const book = findBook(req, res);
 
+  book.title = book3.title;
+  book.type = book3.type;
+  book.author = book3.author;
 
-module.exports = router
+  return res.json({ book });
+});
+
+router.delete("/:id", (req, res) => {
+  const book = findBook(req, res);
+
+  const bookIndex = data.books.indexOf(book);
+
+  data.books.splice(bookIndex, 1);
+
+  return res.json({ book });
+});
+
+module.exports = router;
