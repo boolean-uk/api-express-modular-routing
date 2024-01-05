ceb9defb2478ec490f77cb53bb241963d2659834
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-05 17:00:39 +0100

    add post user endpoint

--- src/routers/users.js
+++ src/routers/users.js
@@ -13,8 +13,13 @@ class User {
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) =>
-  res.json({ users })
-)
+router.get('/', (req, res) => res.json({ users }))
+
+router.post('/', (req, res) => {
+  const { email } = req.body
+  const user = new User(email)
+  users.push(user)
+  res.status(201).json({ user })
+})
 
 module.exports = router
