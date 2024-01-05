b8d239e7c0f02d5b6942451b578a5943df7494d0
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-05 19:26:45 +0000

    import and display all films

--- src/routers/films.js
+++ src/routers/films.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const {films} = require('../../data/index.js')
+
+router.get('/', (req, res) => {
+  res.status(200).json({films})
+})
+
 module.exports = router
