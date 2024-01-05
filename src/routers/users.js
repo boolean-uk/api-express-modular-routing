0af19ed6934ef2abe055b6821ab7edc1cfcc54ad
Lukas Dembicki <lad_777@hotmail.com>
2024-01-05 12:32:21 +0000

    ADD: POST (create a new user) TEST PASSING

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,8 +3,20 @@ const { users } = require('../../data/index.js')
 const express = require('express')
 const router = express.Router()
 
+let userCounter = 3
+
 router.get('/', (req, res) => {
     return res.status(200).json({ users: users })
+})
+
+router.post('/', (req, res) => {
+    let newUser = req.body
+    newUser = {id: ++userCounter, ...newUser}
+    users.push(newUser)
+
+
+
+    return res.status(201).json({user: newUser})
 })
 
 
