221d283a8892a5ef4ca03d0bea8d68bf5288fcbb
Lukas Dembicki <lad_777@hotmail.com>
2024-01-05 12:46:57 +0000

    FORMAT

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,45 +1,49 @@
-const { users } = require('../../data/index.js')
+const { users } = require("../../data/index.js");
 
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-let userCounter = 3
+let userCounter = 3;
 
-router.get('/', (req, res) => {
-    return res.status(200).json({ users: users })
-})
+router.get("/", (req, res) => {
+    return res.status(200).json({ users: users });
+});
 
-function findUserByID (req, res) {
-    const foundUser = users.find((user) => user.id === users.id)
+function findUserByID(req, res) {
+    const foundUser = users.find((user) => user.id === users.id);
 
-    if(!foundUser) return res.status(404).json({ERROR: `A user with ID: ${req.body.id} does not exist`})
-    return foundUser
+    if (!foundUser)
+        return res
+            .status(404)
+            .json({ ERROR: `A user with ID: ${req.body.id} does not exist` });
+    return foundUser;
 }
 
-function emailMatch(newUser){
-    const foundEmail = users.find((email) => email.email === newUser.email)
-    if(foundEmail) return true
-    return false
+function emailMatch(newUser) {
+    const foundEmail = users.find((email) => email.email === newUser.email);
+    if (foundEmail) return true;
+    return false;
 }
 
-router.post('/', (req, res) => {
-    let newUser = req.body
+router.post("/", (req, res) => {
+    let newUser = req.body;
 
-    if(!newUser.email) return res.status(400).json({ERROR: 'Missing fields in request body'})
-    if(emailMatch(newUser)) return res.status(409).json({ERROR: 'A user with the provided email already exists'})
+    if (!newUser.email) {
+        return res
+            .status(400)
+            .json({ ERROR: "Missing fields in request body" });
+    }
 
-    newUser = {id: ++userCounter, ...newUser}
-    users.push(newUser)
+    if (emailMatch(newUser)) {
+        return res
+            .status(409)
+            .json({ ERROR: "A user with the provided email already exists" });
+    }
 
-    return res.status(201).json({user: newUser})
-})
+    newUser = { id: ++userCounter, ...newUser };
+    users.push(newUser);
 
+    return res.status(201).json({ user: newUser });
+});
 
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
