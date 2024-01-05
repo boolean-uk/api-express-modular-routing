95bca735412c09a3aae4b3ea259ff352ba8f080e
callumhayden <callum.hayden@gmail.com>
2024-01-05 12:09:14 +0000

    did get request to receive all users data

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,6 +1,13 @@
 // Import data here...
 const express = require('express')
+const {users} = require("../../data/index")
 const router = express.Router()
+
+
+// Get request for all films 
+router.get("/", (req, res) => {
+res.status(200).json({users: users})
+})
 
 // Write routes here...
 module.exports = router
