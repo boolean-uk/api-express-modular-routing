593011a13090c682230c839d8d914792971be1b8
Lukas Dembicki <lad_777@hotmail.com>
2024-01-05 12:44:41 +0000

    UPDATE: POST (two guard clauses checking for missing email or existing email)

--- src/routers/users.js
+++ src/routers/users.js
@@ -9,12 +9,27 @@ router.get('/', (req, res) => {
     return res.status(200).json({ users: users })
 })
 
+function findUserByID (req, res) {
+    const foundUser = users.find((user) => user.id === users.id)
+
+    if(!foundUser) return res.status(404).json({ERROR: `A user with ID: ${req.body.id} does not exist`})
+    return foundUser
+}
+
+function emailMatch(newUser){
+    const foundEmail = users.find((email) => email.email === newUser.email)
+    if(foundEmail) return true
+    return false
+}
+
 router.post('/', (req, res) => {
     let newUser = req.body
+
+    if(!newUser.email) return res.status(400).json({ERROR: 'Missing fields in request body'})
+    if(emailMatch(newUser)) return res.status(409).json({ERROR: 'A user with the provided email already exists'})
+
     newUser = {id: ++userCounter, ...newUser}
     users.push(newUser)
-
-
 
     return res.status(201).json({user: newUser})
 })
