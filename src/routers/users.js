91d8e9176d69469cc6a02b773cd7aad8a4c85dce
Faiza <faraz_ul_islam@yahoo.com>
2024-01-06 11:19:58 +0000

    get all data from users

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,6 +1,11 @@
 
 const express = require('express')
 const router = express.Router()
+const usersData = require('../../data/index.js')
+
+router.get('/', (req , res) => {
+  return   res.status(200).json({usersData})
+})
 
 
 
