39345d66c2a72cfcf8b93418427dd6c80318c9ab
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-06 22:31:37 +0000

    Add GET endpoint /books

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const { books } = require('../../data/index')
+
+// Global variables
+let bookId = books.length + 1
+
+// Retrieve a list of books
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    books: books
+  })
+})
 
 module.exports = router
