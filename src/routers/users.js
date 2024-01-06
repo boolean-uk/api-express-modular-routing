65220d8a139fbe35c447d1c2a70be66da9f237af
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-06 21:57:46 +0000

    Add POST endpoint /users

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,10 +2,26 @@ const express = require('express')
 const router = express.Router()
 const { users } = require('../../data/index')
 
+// Global variables
+let userId = users.length + 1
+
+// Retrieve a list of users
 router.get('/', (req, res, next) => {
   res.status(200).json({
     users: users
   })
+})
+
+// Create a user
+router.post('/', (req, res, next) => {
+  const createdUser = {
+    id: userId++,
+    email: req.body.email
+  }
+
+  users.push(createdUser)
+
+  res.status(201).json({ user: createdUser })
 })
 
 module.exports = router
