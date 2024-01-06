7d80dfa2a8045415a64cf7963e86ec08aaa639d2
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-06 22:05:18 +0000

    Add DELETE endpoint /users/:id

--- src/routers/users.js
+++ src/routers/users.js
@@ -5,6 +5,13 @@ const { users } = require('../../data/index')
 // Global variables
 let userId = users.length + 1
 
+// Global functions
+const findUserById = (id) => {
+  const foundUser = users.find((user) => user.id === Number(id))
+
+  return foundUser
+}
+
 // Retrieve a list of users
 router.get('/', (req, res, next) => {
   res.status(200).json({
@@ -26,11 +33,23 @@ router.post('/', (req, res, next) => {
 
 // Get a user by ID
 router.get('/:id', (req, res, next) => {
-  const foundUser = users.find((user) => user.id === Number(req.params.id))
+  const foundUser = findUserById(req.params.id)
 
   res.status(200).json({
     user: foundUser
   })
+})
+
+// Delete a user by ID
+router.delete('/:id', (req, res, next) => {
+  const foundUser = findUserById(req.params.id)
+
+  users.splice(
+    users.findIndex((user) => user.id === foundUser.id),
+    1
+  )
+
+  res.status(200).json({ user: foundUser })
 })
 
 module.exports = router
