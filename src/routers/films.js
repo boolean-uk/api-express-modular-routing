c2eed9f0aeb0485b4dab230a23ca4cbcf94af8a5
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-06 22:16:21 +0000

    Add GET endpoing /films

--- src/routers/films.js
+++ src/routers/films.js
@@ -2,4 +2,14 @@ const express = require('express')
 const router = express.Router()
 const { films } = require('../../data/index')
 
+// Global variables
+const filmId = films.length + 1
+
+// Retrieve a list of films
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    films: films
+  })
+})
+
 module.exports = router
