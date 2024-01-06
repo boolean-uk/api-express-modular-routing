bb11f50d0d5bbeaa2eb0fd8371a5e1bbf8aae874
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-06 22:19:21 +0000

    Add POST endpoint /films

--- src/routers/films.js
+++ src/routers/films.js
@@ -3,12 +3,29 @@ const router = express.Router()
 const { films } = require('../../data/index')
 
 // Global variables
-const filmId = films.length + 1
+let filmId = films.length + 1
 
 // Retrieve a list of films
 router.get('/', (req, res, next) => {
   res.status(200).json({
     films: films
+  })
+})
+
+// Create a film
+router.post('/', (req, res, next) => {
+  const { title, director } = req.body
+
+  const createdFilm = {
+    id: filmId++,
+    title,
+    director
+  }
+
+  films.push(createdFilm)
+
+  res.status(201).json({
+    film: createdFilm
   })
 })
 
