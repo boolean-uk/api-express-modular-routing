09d3ff142ac86d01647079291c4dd4dcb7f93b43
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-01-07 22:07:45 +0100

    end point to  Delete request to delete film

--- src/routers/films.js
+++ src/routers/films.js
@@ -45,5 +45,18 @@ router.put("/:id", (req, res) => {
 
   res.status(200).json({ film: foundFilm });
 });
+// Delete request to delete film
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundFilmIndex = films.findIndex((film) => film.id === id);
+
+  if (foundFilmIndex === -1) {
+    return res.status(404).json({ error: `No film found with id ${id}` });
+  }
+
+  const deletedFilm = films.splice(foundFilmIndex, 1)[0];
+
+  res.status(200).json({ film: deletedFilm });
+});
 
 module.exports = router;
