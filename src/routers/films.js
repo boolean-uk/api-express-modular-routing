bae7b644d95a4283254fde8e7d0b7cfd37c53f79
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-01-07 22:00:21 +0100

    end point to Get request for a single id

--- src/routers/films.js
+++ src/routers/films.js
@@ -7,3 +7,17 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({ films });
 });
+
+// Get request for a single id
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const film = films.find((film) => film.id === id);
+
+  if (film) {
+    res.status(200).json({ film });
+  } else {
+    res.status(404).json({ error: `No film found with id ${id}` });
+  }
+});
+
+module.exports = router;
