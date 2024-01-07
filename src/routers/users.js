ad93fb4bacc1e483495a84896dadf467e06f4b54
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-07 20:39:07 +0000

    Add 400 error PUT /users/:id

--- src/routers/users.js
+++ src/routers/users.js
@@ -10,9 +10,9 @@ const findUserById = (id) => {
   const foundUser = users.find((user) => user.id === Number(id))
 
   if (!foundUser) {
-    const err = new Error('A user with the provided ID does not exist')
-    err.status = 404
-    throw err
+    const error = new Error('A user with the provided ID does not exist')
+    error.status = 404
+    throw error
   }
 
   return foundUser
@@ -24,6 +24,16 @@ const findUserByEmail = (email) => {
   return foundUser
 }
 
+const fieldsErrorHandling = (field) => {
+  if (!field) {
+    const error = new Error('Missing fields in request body')
+    error.status = 400
+    throw error
+  }
+
+  return field
+}
+
 // Retrieve a list of users
 router.get('/', (req, res, next) => {
   res.status(200).json({
@@ -33,28 +43,30 @@ router.get('/', (req, res, next) => {
 
 // Create a user
 router.post('/', (req, res, next) => {
-  const { email } = req.body
+  try {
+    const { email } = req.body
 
-  if (!email) {
-    return res.status(400).json({ error: 'Missing fields in request body' })
+    fieldsErrorHandling(email)
+
+    const foundUser = findUserByEmail(email)
+
+    if (foundUser) {
+      return res
+        .status(409)
+        .json({ error: 'A user with the provided email already exists' })
+    }
+
+    const createdUser = {
+      id: userId++,
+      email: email
+    }
+
+    users.push(createdUser)
+
+    res.status(201).json({ user: createdUser })
+  } catch (error) {
+    res.status(error.status).json({ error: error.message })
   }
-
-  const foundUser = findUserByEmail(email)
-
-  if (foundUser) {
-    return res
-      .status(409)
-      .json({ error: 'A user with the provided email already exists' })
-  }
-
-  const createdUser = {
-    id: userId++,
-    email: email
-  }
-
-  users.push(createdUser)
-
-  res.status(201).json({ user: createdUser })
 })
 
 // Get a user by ID
@@ -88,11 +100,19 @@ router.delete('/:id', (req, res, next) => {
 
 // Update a user by ID
 router.put('/:id', (req, res, next) => {
-  const foundUser = findUserById(req.params.id)
+  try {
+    const { email } = req.body
 
-  foundUser.email = req.body.email
+    fieldsErrorHandling(email)
 
-  res.status(200).json({ user: foundUser })
+    const foundUser = findUserById(req.params.id)
+
+    foundUser.email = email
+
+    res.status(200).json({ user: foundUser })
+  } catch (error) {
+    res.status(error.status).json({ error: error.message })
+  }
 })
 
 module.exports = router
