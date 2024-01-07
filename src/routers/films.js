3624a91f01722bfd697a8270734830cb1fd4e5bd
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-07 21:07:47 +0000

    Add 404 error DELETE /films/:id

--- src/routers/films.js
+++ src/routers/films.js
@@ -84,14 +84,18 @@ router.get('/:id', (req, res, next) => {
 
 // Delete a film by ID
 router.delete('/:id', (req, res, next) => {
-  const foundFilm = findFilmById(req.params.id)
+  try {
+    const foundFilm = findFilmById(req.params.id)
 
-  films.splice(
-    films.findIndex((film) => film.id === foundFilm.id),
-    1
-  )
+    films.splice(
+      films.findIndex((film) => film.id === foundFilm.id),
+      1
+    )
 
-  res.status(200).json({ film: foundFilm })
+    res.status(200).json({ film: foundFilm })
+  } catch (error) {
+    res.status(error.status).json({ error: error.message })
+  }
 })
 
 // Update a film by ID
