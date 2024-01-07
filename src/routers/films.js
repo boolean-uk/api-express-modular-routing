0209b167e17b78b939fa9fcdc41f0a6021d77120
Loza_TM <lozatmamo@gmail.com>
2024-01-08 00:26:09 +0100

    Handling GET, POST, PUT , Delete , remove and PATCH  requests to retrieve all films

--- src/routers/films.js
+++ src/routers/films.js
@@ -49,4 +49,91 @@ function findFilmsByDirector(req, res) {
 
     return foundFilmsByDirector;
 }
+
+// Handling GET, POST, PUT , Delete , remove and PATCH  requests to retrieve all films
+router.get("/", (req, res) => {
+    // If a director is specified in the query parameters, find films by director
+    if (req.query.director) {
+        const foundFilms = findFilmsByDirector(req, res);
+        return res.status(200).json({ films: foundFilms });
+    }
+
+
+    return res.status(200).json({ films: films });
+});
+
+
+router.post("/", (req, res) => {
+    let newFilm = req.body;
+
+    if (!newFilm || !newFilm.title || !newFilm.director) {
+        return res
+            .status(400)
+            .json({ error: "Missing fields in the request body" });
+    }
+    if (filmMatch(newFilm)) {
+        return res
+            .status(409)
+            .json({ error: "A film with the provided title already exists" });
+    }
+
+    // Assign a unique ID and add the new film to the array
+    newFilm = { id: ++filmCounter, ...newFilm };
+    films.push(newFilm);
+    return res.status(201).json({ film: newFilm });
+});
+router.get("/:id", (req, res) => {
+    const foundFilm = findFilmByID(req, res);
+    return res.status(200).json({ film: foundFilm });
+});
+router.delete("/:id", (req, res) => {
+    const foundFilm = findFilmByID(req, res);
+    const foundFilmIndex = films.indexOf(foundFilm);
+
+    // Remove the film from the array
+    films.splice(foundFilmIndex, 1);
+
+    // Return the deleted film
+    return res.status(200).json({ film: foundFilm });
+});
+router.put("/:id", (req, res) => {
+    const foundFilm = findFilmByID(req, res);
+    const updateInfo = req.body;
+    if (!updateInfo || !updateInfo.title) {
+        return res
+            .status(400)
+            .json({ error: "Missing fields in the request body" });
+    }
+    if (filmMatch(updateInfo)) {
+        return res
+            .status(409)
+            .json({ error: "A film with the provided title already exists" });
+    }
+
+    // Update the film with the new information
+    foundFilm.title = updateInfo.title;
+    foundFilm.director = updateInfo.director;
+    return res.status(200).json({ film: foundFilm });
+});
+
+// Handle PATCH request to partially update a film by ID
+router.patch("/:id", (req, res) => {
+    const foundFilm = findFilmByID(req, res);
+    const { title, director } = req.body;
+    if (title === "" || director === "" || !req.body) {
+        return res
+            .status(400)
+            .json({ error: "Missing fields in the request body" });
+    }
+    if (filmMatch(req.body)) {
+        return res
+            .status(409)
+            .json({ error: "A film with the provided title already exists" });
+    }
+
+    foundFilm.title = title ? title : foundFilm.title;
+    foundFilm.director = director ? director : foundFilm.director;
+    return res.status(200).json({ film: foundFilm });
+});
+
 module.exports = router;
