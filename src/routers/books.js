30dd13aeb12b91a9eba3e2eaf04e0b983129a3c0
Loza_TM <lozatmamo@gmail.com>
2024-01-08 00:13:10 +0100

    Defining a function and check if a book exist as newbook

--- src/routers/books.js
+++ src/routers/books.js
@@ -2,3 +2,17 @@
 const { books } = require('../../data/index.js')
 
 // Write routes here...
+const express = require('express')
+const router = express.Router()
+
+let bookCounter = 4
+//Defining a function named bookMatch that takes a parameter newBook
+function bookMatch(newBook) {
+    const foundBook = books.find((book) => book.title === newBook.title);
+    //check if a book with the same title as newBook already exists
+    if (foundBook) return true;
+    return false;
+}
+
+
+module.exports = router
