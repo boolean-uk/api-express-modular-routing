8f9aa51a308711c444b4559e8fbf3069ae9e5644
atiqur rahman <atiq.ar@gmail.com>
2024-01-07 21:02:11 +0000

    post

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const data = require('../../data/index.js')
-const users = data.users
-router.get("/", (req, res) => {
-  res.status(200).json({users:users });
+const data = require("../../data/index.js");
+const currentUserId = 4;
+const users = data.users;
+
+res.status(200).json({ users: users });
+
+router.post("/", (req, res) => {
+  const { email } = req.body;
+  const newUser = {
+    id: currentUserId,
+    email,
+  };
+  users.push(newUser);
+  res.status(201).json({ user: newUser });
 });
 module.exports = router;
