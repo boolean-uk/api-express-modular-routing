403d5931795346b9d2085fb3147d057c4fe7f73f
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-12 01:39:02 +0100

    added post req to add new film

--- src/routers/films.js
+++ src/routers/films.js
@@ -7,12 +7,22 @@ const data = require("../../data/index.js");
 
 const films = data.films;
 
-
-
 // Write routes here...
 router.get("/", (req, res) => {
-  res.status(200).json({ films : films });
+  res.status(200).json({ films: films });
 });
 
+router.post("/", (req, res) => {
+  const filmInfo = req.body;
+
+  const newFilm = {
+    id: films.length + 1,
+    title: filmInfo.title,
+    director: filmInfo.director,
+  };
+
+  films.push(newFilm);
+  res.status(201).json({ newFilm });
+});
 
 module.exports = router;
