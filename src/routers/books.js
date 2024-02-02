92455556ff5019601fe282450d969ed67dad5897
Radio58 <rt58gm@gmail.com>
2024-02-02 13:17:11 +0000

    book post added

--- src/routers/books.js
+++ src/routers/books.js
@@ -7,4 +7,11 @@ router.get('/', (req, res) => {
     res.json({ books })
 })
 
+router.post('/', (req, res) => {
+    const body = req.body
+    const newBook = { id: books.length + 1, ...body }
+    books.push(newBook)
+    res.status(201).json({ book: newBook })
+})
+
 module.exports = router;
