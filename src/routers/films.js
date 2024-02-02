299b27396a7db0b587e8e668b69fe0c443c9280b
Radio58 <rt58gm@gmail.com>
2024-02-02 16:22:23 +0000

    fixed naming

--- src/routers/films.js
+++ src/routers/films.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 const findFilm = (id) => {
-    return films.find(user => user.id === parseInt(id))
+    return films.find(film => film.id === parseInt(id))
 }
 
 router.get('/', (req, res) => {
@@ -15,35 +15,35 @@ router.post('/', (req, res) => {
     const body = req.body
     const newFilm = { id: films.length + 1, ...body }
     films.push(newFilm)
-    res.status(201).json({ user: newFilm })
+    res.status(201).json({ film: newFilm })
 })
 
 router.get('/:id', (req, res) => {
-    const user = findFilm(req.params.id)
-    if (user) {
-        res.json({ user })
+    const film = findFilm(req.params.id)
+    if (film) {
+        res.json({ film })
     } else {
-        res.status(404).json({ error: 'user not found' })
+        res.status(404).json({ error: 'film not found' })
     }
 })
 
 router.delete('/:id', (req, res) => {
-    const user = findFilm(req.params.id)
-    if (user) {
-        films.splice(films.indexOf(user), 1)
-        res.json({ user })
+    const film = findFilm(req.params.id)
+    if (film) {
+        films.splice(films.indexOf(film), 1)
+        res.json({ film })
     } else {
-        res.status(404).json({ error: 'user not found' })
+        res.status(404).json({ error: 'film not found' })
     }
 })
 
 router.put('/:id', (req, res) => {
-    const user = findFilm(req.params.id)
-    if (user) {
-        Object.assign(user, req.body)
-        res.json({ user })
+    const film = findFilm(req.params.id)
+    if (film) {
+        Object.assign(film, req.body)
+        res.json({ film })
     } else {
-        res.status(404).json({ error: 'user not found' })
+        res.status(404).json({ error: 'film not found' })
     }
 })
 
